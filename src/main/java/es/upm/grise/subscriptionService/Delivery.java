e0ade17ec4fdea784479842d39603816ce3becc1
odieste <odieste@fi.upm.es>
2026-07-14 09:25:45 +0200

    Initial commit

---
+++ src/main/java/es/upm/grise/subscriptionService/Delivery.java
@@ -0,0 +1,6 @@
+package es.upm.grise.subscriptionService;
+
+public enum Delivery {
+	EMAIL, LOCAL, DO_NOT_DELIVER
+
+}
---
+++ src/main/java/es/upm/grise/subscriptionService/exceptions/ExistingUserException.java
@@ -0,0 +1,5 @@
+package es.upm.grise.subscriptionService.exceptions;
+
+public class ExistingUserException extends Exception {
+
+}
